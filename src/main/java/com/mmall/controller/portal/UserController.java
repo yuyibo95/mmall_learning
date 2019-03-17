ce3aeb3f754146d74a3be0f53ce0e154c9001b9c
yuyibo95 <yuyibo1995@126.com>
2019-03-17 21:08:36 +0800

    commit cookie

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,6 +1,7 @@
 package com.mmall.controller.portal;
 
 import com.mmall.common.Const;
+import com.mmall.common.RedisPool;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
@@ -8,6 +9,7 @@ import com.mmall.service.IUserService;
 import com.mmall.util.Cookieutil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +35,7 @@ public class UserController {
 
     @RequestMapping(value="login.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse, HttpServletRequest httpServletRequest)
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse)
     {
 
         ServerResponse<User> response=iUserService.login(username,password);
@@ -42,9 +44,6 @@ public class UserController {
             //session.setAttribute(Const.CURRENT_USER,response.getData());
             Cookieutil.writeLoginToken(httpServletResponse,session.getId());
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
-
-
-
         }
 
         return response;
@@ -55,8 +54,12 @@ public class UserController {
 
     @RequestMapping(value="logout.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> logout(HttpSession session){
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse){
+        String loginToken =Cookieutil.readLoginToken(httpServletRequest);
+        Cookieutil.delLoginToken(httpServletRequest,httpServletResponse);
+        RedisPoolUtil.del(loginToken);
+
+        //session.removeAttribute(Const.CURRENT_USER);
         return ServerResponse.createBySucess();
 
     }
@@ -80,8 +83,17 @@ public class UserController {
 
     @RequestMapping(value="get_user_info.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session){
-        User user=(User) session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpServletRequest httpServletRequest){
+
+        //User user=(User) session.getAttribute(Const.CURRENT_USER);
+        String loginToken =Cookieutil.readLoginToken(httpServletRequest);
+        if(StringUtils.isEmpty(loginToken)){
+            return ServerResponse.createByErrorMessage("用户未登录，无法获得当前用户的信息");
+        }
+
+        String userJsonStr=RedisPoolUtil.get(loginToken);
+        User user=JsonUtil.string2Obj(userJsonStr,User.class);
+
         if(user!=null){
             return ServerResponse.createBySucess(user);
         }
